efdc2925919d6c810fe2d005fd21e614f64065ed
davique0 <davique@gmail.com>
2023-02-25 09:34:55 -0500

    Storybook button working, classnames library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = classNames(
+    "button",
+    { "button--danger": props.danger },
+    { "button--confirm": props.confirm }
+  );
+
+  //   //conditional to add class when the button is "confirm"
+  //   if (props.confirm) {
+  //     buttonClass += " button--confirm";
+  //   }
+  //   //conditional to add class when the button is "danger or Cancel"
+  //   if (props.danger) {
+  //     buttonClass += " button--danger";
+  //   }
+
+  return (
+    //passing class and actions as a props from inde.js in stories
+    <button
+      onClick={props.onClick}
+      className={buttonClass}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
