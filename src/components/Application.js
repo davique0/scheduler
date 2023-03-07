6c86f7d36ab3043d330b79608940c05d60603b38
davique0 <davique@gmail.com>
2023-03-06 22:10:00 -0500

    Add useVisualMode hook logic

--- src/components/Application.js
+++ src/components/Application.js
@@ -22,9 +22,6 @@ export default function Application(props) {
       axios.get("/api/appointments"),
       axios.get("/api/interviewers"),
     ]).then((all) => {
-      console.log(all[0].data);
-      console.log(all[1].data);
-      console.log(all[2].data);
       setState((prev) => ({
         ...prev,
         days: all[0].data,
