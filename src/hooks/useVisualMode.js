1c2a026d1d8af12aa9dc5c695d2ddc97da393556
davique0 <davique@gmail.com>
2023-03-06 21:28:13 -0500

    Add replace to transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,14 +5,20 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
   //setMode to a new one
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
+    //if replace is true, like in an error, it deletes alst on history and replace it with newMode.
+    if (replace) {
+      setHistory([...history.slice(0, history.length - 1), newMode]);
+      setMode(newMode);
+      return;
+    }
     setMode(newMode);
     setHistory([...history, newMode]);
   };
   //goes back to previous mode
   const back = () => {
     //if history array is already at first value it shouldn't do anything
-    if (history.length === 1) return;
+    if (history.length <= 1) return;
     //create a new array to delete last item of history array and set it as new history
     let newHistory = [...history];
     newHistory.pop();
