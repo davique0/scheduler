ed4d580d6c9e5832837e5863bc7b10421b075938
davique0 <davique@gmail.com>
2023-04-01 10:36:36 +0700

    Add book interview test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -15,14 +15,18 @@ describe("Navigation", () => {
     //1. Visits the root of our web server
     cy.visit("/");
     //2. Clicks on the "Add" button in the second appointment
-    cy.get("img");
+    cy.get(".appointment__add-button").should("be.visible");
+    cy.get("[data-testid=Add]:visible").click();
     //3. Enters their name
+    cy.get("input").type("Mario Gutierrez");
     //4. Chooses an interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
     //5. Clicks the save button
+    cy.contains("Save").click();
     //6. Sees the booked appointment
+    cy.contains(".appointment__card--show", "Mario Gutierrez");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
-
-  // it("should edit an interview");
 
   // it("should cancel an interview");
 });
